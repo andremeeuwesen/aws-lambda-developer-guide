d7cf949ad9326412d760668a38cf3b2669083092
Andre Meeuwesen <andre.meeuwesen@trivento.nl>
2022-02-04 12:54:14 +0100

    update

--- sample-apps/blank-go/1-create-bucket.sh
+++ sample-apps/blank-go/1-create-bucket.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
 BUCKET_NAME=lambda-artifacts-$BUCKET_ID
 echo $BUCKET_NAME > bucket-name.txt
